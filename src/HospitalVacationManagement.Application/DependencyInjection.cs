c03f297b00e7559f76250fdfa41179601938ae07
Alesson Barauna <alesson.barauna1@gmail.com>
2026-07-13 11:28:07 -0300

    Registro do services application

---
+++ src/HospitalVacationManagement.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+using HospitalVacationManagement.Application.Vacations;
+using HospitalVacationManagement.Domain.Vacations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HospitalVacationManagement.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IVacationPolicyService, VacationPolicyService>();
+        services.AddScoped<ValidateVacationRequestHandler>();
+        
+        return services;
+    }
+}
