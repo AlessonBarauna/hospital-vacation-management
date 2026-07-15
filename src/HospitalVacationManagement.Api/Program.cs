b544fb46e9e3ec9316e351f36998ab88b7429c91
Alesson Barauna <alesson.barauna1@gmail.com>
2026-07-15 17:59:02 -0300

    Conectando a API

--- src/HospitalVacationManagement.Api/Program.cs
+++ src/HospitalVacationManagement.Api/Program.cs
@@ -1,44 +1,35 @@
+using HospitalVacationManagement.Application;
+using HospitalVacationManagement.Application.Vacations;
+using HospitalVacationManagement.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapPost("/vacation-requests/validate", async (
+    ValidateVacationRequest request,
+    ValidateVacationRequestHandler handler,
+    CancellationToken cancellationToken) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var response = await handler.HandleAsync(request, cancellationToken);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return response.IsValid
+        ? Results.Ok(response)
+        : Results.BadRequest(response);
 })
-.WithName("GetWeatherForecast")
+.WithName("ValidateVacationRequest")
 .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
